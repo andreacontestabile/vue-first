53c4b46f8e7e18b82ef21ac8b4a2f138be816b78
Andrea Contestabile <andreacontest@gmail.com>
2020-11-13 18:02:04 +0100

    Todo list con aggiunta elementi

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    newTodo: "",
+    todos: [
+      {
+        text: "Comprare il latte"
+      },
+      {
+        text: "Comprare le uova"
+      },
+      {
+        text: "Comprare la farina"
+      }
+    ]
+  },
+  methods: {
+    createTodo: function () {
+      var newTodo = {
+        text: this.newTodo
+      }
+      this.todos.push(newTodo);
+    }
   }
 });
